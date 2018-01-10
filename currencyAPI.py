3a7e8b6a0bbbca32639919c62561e2d6d06906d8
nicoviarnes <nlviarnes@gmail.com>
2018-01-10 11:11:05 -0800

    Update currencyAPI.py
    refactor script

--- currencyAPI.py
+++ currencyAPI.py
@@ -2,37 +2,27 @@ import urllib
 import urllib.request
 from bs4 import BeautifulSoup
 import json
-
-
 def lambda_handler(event, context):
-	
     league = event['league']
     want = event['want']
     have = event['have']
-    if have == '4':
-        curHave = "Chaos"
-
-    if want == '6':
-        curWant = "Exalted"
-
     poe = urllib.request.urlopen('http://currency.poe.trade/search?league='+league+'&online=x&want='+want+'&have='+have).read()
-    soup = BeautifulSoup(poe, "html.parser")
-
+    soup = BeautifulSoup(poe, 'html.parser')
     listings = soup.find_all('div', class_ = 'displayoffer')
     cNames = soup.find_all('div', class_ = 'large-3.small-3.columns.currency')
     jdata = {}
+    json_data = {}
     i = 0
-    while i < 10:
-        ign = listings[i]['data-ign']
-        buyVal = listings[i]['data-buyvalue']
-        sellVal = listings[i]['data-sellvalue']
-        simVal = float(buyVal)/float(sellVal)
-        i+=1
-
-        jdata['ign'] = ign
-        jdata['rate'] = str(simVal) +':1'
-        jdata['have'] = curHave
-        jdata['want'] = curWant
-        json_data = json.dumps(jdata)
-        print(json_data)
-
+    if listings:
+        while i < len(listings):
+            ign = listings[i]['data-ign']
+            buyVal = listings[i]['data-buyvalue']
+            sellVal = listings[i]['data-sellvalue']
+            simVal = float(buyVal)/float(sellVal)
+            i+=1
+            jdata['ign'] = ign
+            jdata['rate'] = str(simVal) +':1'
+            json_data[i] = json.dumps(jdata)
+        return json_data
+    else:
+        return ('Nothing found.')
